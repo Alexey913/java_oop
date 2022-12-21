94ccda0972f220f925d31200b4c22a15b897702e
Alexey <al913@mail.ru>
2022-12-21 23:07:53 +0300

    seminar 2 oop

---
+++ util/ReaderFromTxt.java
@@ -0,0 +1,13 @@
+/* Создать классы WriterToTxt и ReaderFromTxt, имеющие
+ * статичные методы для записи/чтения в/из txt.файла*/
+
+package util;
+
+import data.User;
+
+public class ReaderFromTxt {
+
+    public static User read(User user) {
+        return user;
+    }
+}
---
+++ util/WriterToTxt.java
@@ -0,0 +1,13 @@
+/* Создать классы WriterToTxt и ReaderFromTxt, имеющие
+ * статичные методы для записи/чтения в/из txt.файла*/
+
+package util;
+
+import data.User;
+
+public class WriterToTxt {
+    
+    public static void write(User user) {
+        
+    }
+}
