5ba62f287c2139ca6b0467fb28085a33b084b5d3
Alexey <al913@mail.ru>
2022-12-30 21:54:00 +0300

    seminar 4 rechange final

--- service/StudentGroupServiceImpl.java
+++ service/StudentGroupServiceImpl.java
@@ -1,42 +1,3 @@
-// Д/З Семинар 2
-// 1. Создать класс StudentGroup, содержащая в себе поля Teacher и список студентов.
-// 2. Создать класс StudentGroupServiceImpl, в котором реализована
-// логика чтения Студентов и Преподавателя из файла txt (реализация чтения файла опциональна),
-// создания класса StudentGroup и возвращения его.
-// 3. Создать метод в Controller createGroup(int groupNumber),
-// в который передается номер группы, а возвращается StudentGroup.
-// 4. Все вышеуказанное создать согласно принципам ООП пройдённым на семинаре.
-
-
-// Семинар 3 - Задача 2
-// 1. Модифицировать класс StudentGroup, заставив его реализовать интерфейс Iterable<Student>
-// 2. Реализовать метод iterator() возвращающий экземпляр созданного нами итератора
-// 3. Модифицировать класс StudentGroupServiceImpl, добавив в него метод удаления студента по ФИО
-// 4. Модифицировать класс Controller, добавив в него метод удаления студента
-// и вызывать в нем созданный метод из StudentGroupServiceImpl
-
-
-// Семинар 3 - задача 3
-// 1. Модифицировать класс Student, заставив его реализовать интерфейс Comparable
-// 2. Реализовать контракт compareTo () со сравнением по году рождения студента
-// 3. Модифицировать класс StudentGroupServiceImpl, добавив в него метод сортировки списка студентов
-// 4. Модифицировать класс Controller, добавив в него метод сортировки списка студентов
-// и вызывать в нем созданный метод из StudentGroupServiceImpl
-
-
-// Семинар 3 - Задача 4
-// 1. Создать класс UserComparator реализующий интерфейс Comparator<User>
-// 2. Реализовать контракт compareTo() со сравнением по ФИО
-// 3. Модифицировать класс StudentGroupServiceImpl, добавив в него метод сортировки списка студентов по ФИО
-// 4. Модифицировать класс Controller, добавив в него метод сортировки списка студентов
-// по ФИО и вызывать в нем созданный метод из StudentGroupServiceImpl
-
-
-// Семинар 4 - Задача 2
-// - Создать класс GroupRepository имплементировав его от Repository<Group, Integer>
-// - В классе StudentGroupServiceImpl добавить новую переменную Repository<Group, Integer>
-// - В классе StudentGroupServiceImpl реализовать методы сохранения группы и поиска её по номеру 
-
 
 package service;
 
@@ -50,42 +11,40 @@ import data.Teacher;
 import data.comparator.UserComparator;
 import repository.Repository;
 import util.ReaderFromTxt;
+import util.WriterToTxt;
 
 public class StudentGroupServiceImpl implements StudentGroupService {
 
-// К Семинару 4 - Задача 2
-    private final Repository <StudentGroup, Integer> studentGroupRepository;
+    private final Repository<StudentGroup, Integer> studentGroupRepository;
 
     public StudentGroupServiceImpl(Repository<StudentGroup, Integer> studentGroupRepository) {
         this.studentGroupRepository = studentGroupRepository;
     }
 
-    // К Д/З Семинар 2
     @Override
     public StudentGroup read() {
         Teacher teacher = ReaderFromTxt.readTeacher();
-        List <Student> listOfStudents = ReaderFromTxt.readStudents();
-        StudentGroup studentGroup = new StudentGroup (teacher, listOfStudents);
+        List<Student> listOfStudents = ReaderFromTxt.readStudents();
+        StudentGroup studentGroup = new StudentGroup(teacher, listOfStudents);
         return studentGroup;
     }
 
-    public StudentGroup read (int groupNumber) {
+    public StudentGroup read(int groupNumber) {
         Teacher teacher = ReaderFromTxt.readTeacher();
-        List <Student> listOfStudents = ReaderFromTxt.readStudents();
-        StudentGroup studentGroup = new StudentGroup (teacher, listOfStudents, groupNumber);
+        List<Student> listOfStudents = ReaderFromTxt.readStudents();
+        StudentGroup studentGroup = new StudentGroup(teacher, listOfStudents, groupNumber);
         return studentGroup;
     }
 
     @Override
     public StudentGroup create(StudentGroup studentGroup) {
-        // Запись группы в текстовый файл
+        WriterToTxt.writeStudentGroup(studentGroup);
         return studentGroup;
     }
 
-// К Семинару 3 - Задача 2
     @Override
     public void removeStudent(String fio) {
-        Iterator <Student> studentGroup = read().iterator();
+        Iterator<Student> studentGroup = read().iterator();
         while (studentGroup.hasNext()) {
             Student student = studentGroup.next();
             if (fio.equals(student.getFio())) {
@@ -94,19 +53,16 @@ public class StudentGroupServiceImpl implements StudentGroupService {
         }
     }
 
-// К Семинару 3 - Задача 3
     @Override
     public void sortByYear(StudentGroup studentGroup) {
         Collections.sort(studentGroup.getListOfStudents());
     }
 
-// К Семинару 3 - Задача 4
     @Override
     public void sortByFio(StudentGroup studentGroup) {
         Collections.sort(studentGroup.getListOfStudents(), new UserComparator());
     }
 
-// К Семинару 4 - Задача 2
     @Override
     public StudentGroup saveGroup(StudentGroup studentGroup) {
         return studentGroupRepository.save(studentGroup);
